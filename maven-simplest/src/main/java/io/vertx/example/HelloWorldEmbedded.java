5dfad4bd13115cfd79eab88107e6bcde06d24ea7
Li Chen <kidneychenli@gmail.com>
2015-10-23 01:29:28 -0400

    Add jdbc feature to enabling accesses to mysql

--- maven-simplest/src/main/java/io/vertx/example/HelloWorldEmbedded.java
+++ maven-simplest/src/main/java/io/vertx/example/HelloWorldEmbedded.java
@@ -4,6 +4,11 @@ import io.vertx.core.Vertx;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.mysql.jdbc.Statement;
 
 /**
  * @author <a href="http://tfox.org">Tim Fox</a>
@@ -11,6 +16,42 @@ import java.util.Date;
 public class HelloWorldEmbedded {
 
   public static void main(String[] args) {
+
+
+    /********************mysql test start****************/
+    try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+    } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+        
+    String url = "jdbc:mysql://localhost:3306/mysql";
+    String userName = "root";
+    String passWord = "coding15619";
+        
+    Connection con = null;
+    try {
+        con = DriverManager.getConnection(url, userName, passWord);
+    } catch (SQLException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+    try {
+        Statement sql_statement = (Statement) con.createStatement();
+        String query = "select * from user";
+        ResultSet result = sql_statement.executeQuery(query);
+        while (result.next()) {
+            String res = result.getString("Host");
+            System.out.println(res);
+        }
+    } catch (SQLException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+    /*********************mysql test end*****************/
+
+
     // Create an HTTP server which simply returns "Hello World!" to each request.
     PhaistosDiscCipher pdc = new PhaistosDiscCipher("8271997208960872478735181815578166723519929177896558845922250595511921395049126920528021164569045773");
     String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
