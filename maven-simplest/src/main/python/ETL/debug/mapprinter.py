c37b1fd6e2fea46236de59219d23fe6487ad530d
Eric Haibin Lin <linhaibin.eric@gmail.com>
2015-10-26 13:52:06 -0400

    fix bug in map printer.

--- maven-simplest/src/main/python/ETL/debug/mapprinter.py
+++ maven-simplest/src/main/python/ETL/debug/mapprinter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import json
+import sys
 
 for line in sys.stdin:
 	print line
---
+++ maven-simplest/src/main/python/ETL/debug/reducerloader.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+import os
+import happybase
+import json
+
+TABLE_NAME = "tweets"
+DELIMITER = "<15619_delimiter>"
+NEWLINE = "<15619_newline>"
+#Remember to modify the DNS address of HBase master
+DNS = "???"
+connection = happybase.Connection(DNS)
+
+def create_table(table):
+	global connection
+	try:
+		families = {'info': dict(in_memory=True, block_cache_enabled=True)}
+		connection.create_table(table, families)
+	except Exception, e:
+		pass
+
+def main():
+	create_table(TABLE_NAME)
+	table = connection.table(TABLE_NAME)
+	with table.batch(batch_size=1000) as tweet_batch:
+		for line in sys.stdin:
+			try:
+				tweet = json.loads(line)
+				uid = tweet['uid'].encode('utf-8')
+				tid = tweet['tid'].encode('utf-8')
+				score = tweet['score'].encode('utf-8')
+				time = tweet['time'].encode('utf-8')
+				epoch = tweet['epoch'].encode('utf-8')
+				text = tweet['text'].encode('utf-8')
+				tweet_batch.put(
+					uid + time + tid,
+					{
+						'info:score': 		score,
+						'info:text': 		text,
+						'info:uid' : 		uid,
+						'info:tid': 		tid,
+						'info:timestamp': 	time, 
+						'info:epoch': 		epoch,
+					})
+			except Exception, e:
+				pass
+
+main()
