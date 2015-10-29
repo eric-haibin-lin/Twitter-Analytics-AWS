5787078863e4fee8344edb47c70d44857b9b07ee
Eric Haibin Lin <linhaibin.eric@gmail.com>
2015-10-29 17:29:35 -0400

    + mysql handler. tested locally.

--- maven-simplest/src/main/java/io/vertx/example/MysqlHandler.java
+++ maven-simplest/src/main/java/io/vertx/example/MysqlHandler.java
@@ -9,6 +9,7 @@ import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
+import java.sql.*;
 
 /**
  * @author <a href="http://tfox.org">Tim Fox</a>
@@ -17,47 +18,43 @@ public class MysqlHandler implements DataHandler {
 
   private static final String userName = "root";
   private static final String passWord = "coding15619";
-  private static final String MYSQL_URL = "jdbc:mysql://ec2-54-173-14-165.compute-1.amazonaws.com:3306/tweet";
+  private static final String MYSQL_URL = "jdbc:mysql://localhost:3306/employees";
 
+  public MysqlHandler() {
+    try {
+      Class.forName("com.mysql.jdbc.Driver").newInstance();
+    } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
 
   @Override
   public String getQuery2(String userId, String tweetTime) {
-
-//    try {
-//      Class.forName("com.mysql.jdbc.Driver").newInstance();
-//    } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-//      e.printStackTrace();
-//    }
-//
-//    String url = "jdbc:mysql://localhost:3306/tweet";
-//    Connection conn = null;
-//    try {
-//        conn = DriverManager.getConnection(url, userName, passWord);
-//    } catch (SQLException e) {
-//        e.printStackTrace();
-//    }
-//    final Connection con = conn;
-//
-//    try {
-//        Statement sql_statement = (Statement) con.createStatement();
-//        System.out.println(tweetTime);
-//        String query = "SELECT tid, score, text FROM tweet WHERE uid = '" + userId + "' AND timestamp = '" + tweetTime + "'";
-//        ResultSet result = sql_statement.executeQuery(query);
-//
-//        while (result.next()) {
-//            String uid = result.getString("tid");
-//            String score = result.getString("score");
-//            String text = result.getString("text");
-//            System.out.println(uid + score + text);
-//
-//            resString += uid + ":" + score + ":" + text + "\n";
-//        }
-//    } catch (SQLException e) {
-//        e.printStackTrace();
-//    }
-
-
-    return null;
+    String resString = "";
+    Connection conn = null;
+    try {
+        conn = DriverManager.getConnection(MYSQL_URL, userName, passWord);
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    final Connection con = conn;
+    try {
+        Statement sql_statement = con.createStatement();
+        System.out.println(tweetTime);
+        String query = "SELECT tid, score, text FROM tweet WHERE uid = '" + userId + "' AND timestamp = '" + tweetTime + "'";
+        ResultSet result = sql_statement.executeQuery(query);
+        //TODO add json parsing part
+        while (result.next()) {
+            String uid = result.getString("tid");
+            String score = result.getString("score");
+            String text = result.getString("text");
+            System.out.println(uid + score + text);
+            resString += uid + ":" + score + ":" + text + "\n";
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    return resString;
   }
 }
 
