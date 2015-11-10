be1892d5aa3523d96035fd5661750c29e6a976af
Hechao Li <hechaol@cmu.edu>
2015-11-10 18:15:03 -0400

    MysqlHandler Add q4
    Former-commit-id: f508897593b0a58fce31ab6efae9e6c66f4d40e2

--- maven-simplest/src/main/java/io/vertx/example/MysqlHandler.java
+++ maven-simplest/src/main/java/io/vertx/example/MysqlHandler.java
@@ -20,73 +20,105 @@ import org.apache.commons.dbcp2.BasicDataSource;
  */
 public class MysqlHandler implements DataHandler {
 
-  private static final String userName = "root";
-  private static final String passWord = "coding15619";
-  private static final String MYSQL_URL = "jdbc:mysql://localhost:3306/tweet";
+	private static final String userName = "root";
+	private static final String passWord = "coding15619";
+	private static final String MYSQL_URL = "jdbc:mysql://localhost:3306/tweet";
 
-  private static final DataSource ds = DBCPDataSourceFactory.getDataSource();
-  
-  public static class DBCPDataSourceFactory {
-    public static DataSource getDataSource(){
-      BasicDataSource ds = new BasicDataSource();
-      ds.setDriverClassName("com.mysql.jdbc.Driver");
-      ds.setUrl(MYSQL_URL);
-      ds.setUsername(userName);
-      ds.setPassword(passWord);
+	private static final DataSource ds = DBCPDataSourceFactory.getDataSource();
 
-      return ds;
-    }
-  }
+	public static class DBCPDataSourceFactory {
+		public static DataSource getDataSource() {
+			BasicDataSource ds = new BasicDataSource();
+			ds.setDriverClassName("com.mysql.jdbc.Driver");
+			ds.setUrl(MYSQL_URL);
+			ds.setUsername(userName);
+			ds.setPassword(passWord);
 
-  public MysqlHandler() {
-  }
+			return ds;
+		}
+	}
 
-  public static Connection getConnection() {
-    Connection conSql = null;
-    try {
-        conSql = DriverManager.getConnection(MYSQL_URL, userName, passWord);
-    } catch (SQLException e) {
-	e.printStackTrace();
-    }
-    
-    return conSql;
-  }
+	public MysqlHandler() {
+	}
 
-  @Override
-  public String getQuery4(String hashtag, Integer n){
-	  return "Not Implement";
-  }
-  
-  @Override
-  public String getQuery2(String userId, String tweetTime) {
-    String resString = "";
-    try {
-        Connection con = ds.getConnection();
+	public static Connection getConnection() {
+		Connection conSql = null;
+		try {
+			conSql = DriverManager.getConnection(MYSQL_URL, userName, passWord);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-        Statement sql_statement = con.createStatement();
-        String q = userId + "_" + tweetTime;
-        String query = "SELECT r FROM q2 WHERE q = '" + q +"'";
-        ResultSet result = sql_statement.executeQuery(query);
+		return conSql;
+	}
 
-        if (result.next()) {
-            
-            Blob b = result.getBlob("r");
-            long l = b.length();
-            byte[] bytes = b.getBytes(1, (int) l);
-            resString = new String(bytes);
+	@Override
+	public String getQuery4(String hashtag, Integer n) {
+		String resString = "";
+		try {
+			Connection con = ds.getConnection();
 
-        }
+			Statement sql_statement = con.createStatement();
+			String q = hashtag;
+			String query = "SELECT r FROM q4 WHERE q = '" + q + "'";
+			ResultSet result = sql_statement.executeQuery(query);
 
-        if (result != null) result.close();
-        if(sql_statement != null) sql_statement.close();
-        if(con != null) con.close();
+			if (result.next()) {
 
-    } catch (SQLException e) {
-        e.printStackTrace();
-    }
-    return resString;
-  }
+				Blob b = result.getBlob("r");
+				long l = b.length();
+				byte[] bytes = b.getBytes(1, (int) l);
+				String record = new String (bytes);
+				String[] lines = record.split("\b");
+				resString = "";
+				for (int i = 0; i < lines.length && i < n; i++) {
+					resString += lines[i] + "\n";
+				}
+			}
+
+			if (result != null)
+				result.close();
+			if (sql_statement != null)
+				sql_statement.close();
+			if (con != null)
+				con.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return resString;
+	}
+
+	@Override
+	public String getQuery2(String userId, String tweetTime) {
+		String resString = "";
+		try {
+			Connection con = ds.getConnection();
+
+			Statement sql_statement = con.createStatement();
+			String q = userId + "_" + tweetTime;
+			String query = "SELECT r FROM q2 WHERE q = '" + q + "'";
+			ResultSet result = sql_statement.executeQuery(query);
+
+			if (result.next()) {
+
+				Blob b = result.getBlob("r");
+				long l = b.length();
+				byte[] bytes = b.getBytes(1, (int) l);
+				resString = new String(bytes);
+
+			}
+
+			if (result != null)
+				result.close();
+			if (sql_statement != null)
+				sql_statement.close();
+			if (con != null)
+				con.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return resString;
+	}
 }
-
-
-
